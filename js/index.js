186b4f79fb423a4d7f1783e3fa65e4ba4facaf3b
mkasiam <kawsarahmmed10001@gmail.com>
2023-09-01 00:08:31 +0600

    It's done for 31 augest

--- js/index.js
+++ js/index.js
@@ -36,15 +36,17 @@ const loadVideoContentHandler = (arrayOfVideos) =>{
   videoContainer.textContent="";
     arrayOfVideos.forEach((element) => {
         const authorsInfo = element.authors[0];
-        const viewsInSecond = parseFloat(element.others.posted_date);
-        console.log(viewsInSecond);
+        let viewsInSecond = parseFloat(element.others.posted_date);
+        viewsInSecond = secondsToHoursMinute(viewsInSecond);
+        const verifiedCheck = authorsInfo.verified;
+        verifiedBadgeHandler(verifiedCheck);
         const div = document.createElement("div");
         div.innerHTML = `
             <div class="flex justify-center items-center">
             <div class="max-w-md rounded-lg overflow-hidden shadow-lg bg-white">
               <div class="relative">
                 <img src="${element.thumbnail}" alt="Video Thumbnail" class="w-full h-48 object-cover">
-                <span class="absolute bottom-2 right-2 bg-black text-white px-2 py-1 rounded opacity-75">10:25</span>
+                <span class="absolute bottom-2 right-2 bg-black text-white px-2 py-1 rounded opacity-75">${viewsInSecond?viewsInSecond:""}</span>
               </div>
               <div class="p-4">
                 <div class="flex items-center">
@@ -53,7 +55,7 @@ const loadVideoContentHandler = (arrayOfVideos) =>{
                     <h2 class="text-xl font-semibold">${element.title}</h2>
                     <div class="flex items-center mt-1">
                     <p class="text-gray-600">${authorsInfo.profile_name}</p>
-                    <span class="bg-blue-500 text-white text-xs px-2 py-1 rounded-full mr-1">
+                    <span id="verifiedBadge" class="hidden bg-blue-500 text-white text-xs px-2 py-1 rounded-full mr-1">
                     <svg xmlns="http://www.w3.org/2000/svg" width="10" height="8" viewBox="0 0 10 8" fill="none">
                     <path d="M7.7094 1.20637L4.14065 4.77512L2.29065 2.92668C1.88909 2.52512 1.23752 2.52512 0.835962 2.92668C0.434399 3.32824 0.434399 3.97981 0.835962 4.38137L3.43127 6.97668C3.8219 7.36731 4.45627 7.36731 4.8469 6.97668L9.16253 2.66106C9.56409 2.25949 9.56409 1.60793 9.16253 1.20637C8.76096 0.804807 8.11096 0.804807 7.7094 1.20637Z" fill="#FFFCEE"/>
                     </svg>
@@ -73,8 +75,27 @@ const loadVideoContentHandler = (arrayOfVideos) =>{
 
 }
 
+function secondsToHoursMinute(seconds) {
+    if(isNaN(seconds)){
+        return;
+    }
+    else{
+        const hr = Math.floor(seconds / 3600);
+        const min = Math.floor((seconds % 3600) / 60);
+    
+        return(`${hr} hrs ${min} min ago`);
+    }
+}
 
-
+function verifiedBadgeHandler(isVerified){
+    const verifiedElement = document.getElementById("verifiedBadge");
+    if(isVerified){
+        verifiedElement.classList.remove("hidden");
+    }
+    else{
+        verifiedElement.classList.add("hidden");
+    }
+}
 
 
 // calling load data function

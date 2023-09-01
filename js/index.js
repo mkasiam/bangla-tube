2b7b34149e5dd46f40212f61323b235b7da45e4a
mkasiam <kawsarahmmed10001@gmail.com>
2023-09-01 10:31:46 +0600

    start working on blank page

--- js/index.js
+++ js/index.js
@@ -38,7 +38,6 @@ const loadVideoContentHandler = (arrayOfVideos) => {
     let viewsInSecond = parseFloat(element.others.posted_date);
     viewsInSecond = secondsToHoursMinute(viewsInSecond);
     const verifiedCheck = authorsInfo?.verified;
-    verifiedBadgeHandler(verifiedCheck); 
     const div = document.createElement("div");
     div.innerHTML = `
             <div class="flex justify-center items-center">
@@ -60,20 +59,7 @@ const loadVideoContentHandler = (arrayOfVideos) => {
                     <h2 class="text-xl font-semibold">${element.title}</h2>
                     <div class="flex items-center mt-1 gap-2">
                     <p class="text-gray-600">${authorsInfo.profile_name}</p>
-                    <div id="verifiedBadge" class="hidden bg-blue-500 text-white  px-1 py-1 rounded-full mr-1">
-                        <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="10"
-                        height="8"
-                        viewBox="0 0 10 8"
-                        fill="none"
-                      >
-                        <path
-                          d="M7.7094 1.20626L4.14065 4.77501L2.29065 2.92657C1.88909 2.52501 1.23752 2.52501 0.835962 2.92657C0.434399 3.32814 0.434399 3.9797 0.835962 4.38126L3.43127 6.97658C3.8219 7.3672 4.45627 7.3672 4.8469 6.97658L9.16253 2.66095C9.56409 2.25939 9.56409 1.60782 9.16253 1.20626C8.76096 0.8047 8.11096 0.8047 7.7094 1.20626Z"
-                          fill="#FFFCEE"
-                        />
-                      </svg>
-                    </div>
+                    <div id="verifiedBadge" >${verifiedCheck}</div>
                     </div>
                     <p class="text-gray-500">${element.others.views}</p>
                   </div>
@@ -99,19 +85,6 @@ function secondsToHoursMinute(seconds) {
   }
 }
 
-function verifiedBadgeHandler(isVerified){
-  
-  const verifiedBadgeContainer = document.getElementById("verifiedBadge");
-  if(isVerified === true){
-    verifiedBadgeContainer.classList.remove("hidden");
-  }
-  else{
-    verifiedBadgeContainer.classList.add("hidden");
-  }
-  
-  
-
-}
 
 
 
